0b51ca4a54992f25f711cb6944137709d8e81462
kozak127 <kozak127@bitbucket.org>
2014-06-10 22:24:59 +0200

    change names to simpler

--- textPreprocessor/prepareAbstractInternational.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-inputFile="$1"
-outputFile="abstractInternational_${1}"
-python textReplacer.py ./prepareAbstractInternational.conf < $inputFile > tmp0
-python textReplacer.py ./htmlChemicalIndex.conf < tmp0 > $outputFile
-rm tmp*
--- textPreprocessor/prepareInt.sh
+++ textPreprocessor/prepareInt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 inputFile="$1"
-outputFile="txt_${1}"
-python textReplacer.py ./prepareArticle.conf < $inputFile > tmp0
+outputFile="int_${1}"
+python textReplacer.py ./prepareInt.conf < $inputFile > tmp0
 python textReplacer.py ./htmlChemicalIndex.conf < tmp0 > $outputFile
 rm tmp*
